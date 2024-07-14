c6047e5a699bfc0d19efbe1d8a01790e599cf8ce
Mastermind730 <sourav.97032@gmail.com>
2024-07-14 21:47:52 +0530

    fix

--- src/config.js
+++ src/config.js
@@ -1,11 +1,6 @@
 const config = {
-<<<<<<< HEAD
-  // serverURL: 'https://bananabackend.onrender.com',?
-  serverURL: 'http://localhost:4000',
-=======
   serverURL: 'https://localhost:4000',
   // serverURL: 'https://bananabackend.onrender.com',
->>>>>>> bee11f1814b2606006bce35424608d9d3a8fde4e
 }
 
 export default config
